a5761f0423f69cf86e1c92a51ca085eaa19f7917
doctamayot <doctamayot@hotmail.com>
2019-04-29 18:49:27 -0500

    heroku o

--- libs/db.js
+++ libs/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
     
     module.exports = function Connection() {
         if(!db){
-            db = mongoose.createConnection('mongodb://localhost:27017/polls');
+            db = mongoose.createConnection(process.env.MONGODB);
             useMongoClient: true;
         }
         return db;
