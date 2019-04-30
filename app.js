0f3500d52ade82a58ad321bd080793073b3b2d0e
doctamayot <doctamayot@hotmail.com>
2019-04-29 20:07:02 -0500

    server

--- app.js
+++ app.js
@@ -43,15 +43,17 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/shop',{ useNewUrlParser: true 
+mongoose.connect('mongodb://localhost:27017/shop',{ useNewUrlParser: true 
 }).then(() => {
     console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
-app.listen(process.env.PORT || 3000, ()=>{
-  console.log('API REST corriendo en http://localhost')
+
+app.set('port', process.env.PORT || 3000 )
+app.listen(app.get('port'), ()=>{
+  console.log(`API REST corriendo en ${app.get('port')}`)
 })
 
 // catch 404 and forward to error handler
@@ -70,85 +72,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-/**
- * Get port from environment and store in Express.
- */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
 
 module.exports = app;
