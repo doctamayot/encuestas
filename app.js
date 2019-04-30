3dc5a47b4870c44554e686f36e8db3a99cb52235
doctamayot <doctamayot@hotmail.com>
2019-04-29 19:12:35 -0500

    hku

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const favicon = require('serve-favicon');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const flash = require('connect-flash');
